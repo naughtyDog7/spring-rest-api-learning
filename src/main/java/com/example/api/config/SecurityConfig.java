b1aa0b79b1d7f1405f65c4e057a26965eec229c4
Elyor <e.hadjihanov@gmail.com>
2020-07-28 23:25:38 +0500

    added basic auth to rest endpoints

--- src/main/java/com/example/api/config/SecurityConfig.java
+++ src/main/java/com/example/api/config/SecurityConfig.java
@@ -1,20 +1,85 @@
 package com.example.api.config;
 
-//@Configuration
-public class SecurityConfig /*extends WebSecurityConfigurerAdapter*/ {
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.cors()
-//                .and()
-//                .authorizeRequests()
-//                .antMatchers(HttpMethod.GET, "/user/info", "/api/students/**")
-//                .hasAuthority("SCOPE_read")
-//                .antMatchers(HttpMethod.POST, "/api/students")
-//                .hasAuthority("SCOPE_write")
-//                .anyRequest()
-//                .authenticated()
-//                .and()
-//                .oauth2ResourceServer()
-//                .jwt();
-//    }
+import org.apache.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    @Qualifier("AuthEntryPoint")
+    private BasicAuthenticationEntryPoint authenticationEntryPoint;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password(passwordEncoder().encode("userPass"))
+                .authorities("ROLE_USER");
+    }
+
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                    .anyRequest()
+                    .authenticated()
+                .and()
+                    .httpBasic()
+                    .authenticationEntryPoint(authenticationEntryPoint);
+        http.addFilterAfter(new CustomFilter(), BasicAuthenticationFilter.class);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public static class CustomFilter extends GenericFilterBean {
+        @Override
+        public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+
+    @Bean(name = "AuthEntryPoint")
+    public BasicAuthenticationEntryPoint authenticationEntryPoint() {
+        return new BasicAuthenticationEntryPoint() {
+            @Override
+            public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+                response.addHeader(HttpHeaders.WWW_AUTHENTICATE, "Basic realm=\"" + getRealmName() + "\"");
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                PrintWriter writer = response.getWriter();
+                writer.println("HTTP Status 401 - " + authException.getMessage());
+            }
+
+            @Override
+            public void afterPropertiesSet() {
+                setRealmName("uzspring");
+                super.afterPropertiesSet();
+            }
+        };
+    }
 }
