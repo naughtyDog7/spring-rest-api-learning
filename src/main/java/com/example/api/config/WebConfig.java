b30abf6273d2b13d491b0f0f740017197d35ffb8
Elyor <e.hadjihanov@gmail.com>
2020-07-30 19:49:46 +0500

    RestTemplate client digest auth

--- src/main/java/com/example/api/config/WebConfig.java
+++ src/main/java/com/example/api/config/WebConfig.java
@@ -1,67 +1,60 @@
 package com.example.api.config;
 
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.AuthCache;
+import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.protocol.HttpClientContext;
-import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.auth.DigestScheme;
 import org.apache.http.impl.client.BasicAuthCache;
-import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
-import org.springframework.beans.factory.FactoryBean;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.http.client.support.BasicAuthenticationInterceptor;
-import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.net.URI;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+public class WebConfig {
+
+    @Value("${server.port}")
+    private int port;
+
+    @Bean
+    public RestTemplate restTemplate() {
+
+        HttpHost host = new HttpHost("localhost", port, "http");
+        CloseableHttpClient client = HttpClientBuilder.create()
+                .setDefaultCredentialsProvider(credentialsProvider())
+                .useSystemProperties()
+                .build();
+
+        ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactoryDigestAuth(client, host);
+        return new RestTemplate(requestFactory);
     }
 
-    @Component
-    public static class RestTemplateFactory implements FactoryBean<RestTemplate>, InitializingBean {
 
-        private RestTemplate restTemplate;
-
-        @Value("${server.port}")
-        private int port;
-
-        @Override
-        public RestTemplate getObject() {
-            return restTemplate;
-        }
-
-        @Override
-        public Class<?> getObjectType() {
-            return RestTemplate.class;
-        }
-
-        @Override
-        public void afterPropertiesSet() {
-            HttpHost host = new HttpHost("localhost", port, "http");
-            ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactoryBasicAuth(HttpClients.createDefault(), host);
-            restTemplate = new RestTemplate(requestFactory);
-            restTemplate.getInterceptors().add(new BasicAuthenticationInterceptor("user", "userPass"));
-        }
+    private CredentialsProvider credentialsProvider() {
+        CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("user", "userPass"));
+        return credentialsProvider;
     }
 
-    public static class HttpComponentsClientHttpRequestFactoryBasicAuth
+    public static class HttpComponentsClientHttpRequestFactoryDigestAuth
             extends HttpComponentsClientHttpRequestFactory {
         private final HttpHost host;
 
-        public HttpComponentsClientHttpRequestFactoryBasicAuth(HttpClient httpClient, HttpHost host) {
+        public HttpComponentsClientHttpRequestFactoryDigestAuth(HttpClient httpClient, HttpHost host) {
             super(httpClient);
             this.host = host;
         }
@@ -73,8 +66,10 @@ public class WebConfig implements WebMvcConfigurer {
 
         private HttpContext createHttpContext() {
             AuthCache authCache = new BasicAuthCache();
-            BasicScheme basicAuth = new BasicScheme();
-            authCache.put(host, basicAuth);
+            DigestScheme digestAuth = new DigestScheme();
+            digestAuth.overrideParamter("realm", "uzspring");
+            digestAuth.overrideParamter("nonce", "");
+            authCache.put(host, digestAuth);
 
             BasicHttpContext context = new BasicHttpContext();
             context.setAttribute(HttpClientContext.AUTH_CACHE, authCache);
