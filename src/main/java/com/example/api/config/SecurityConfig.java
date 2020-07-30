bc9d4a565f01daba217e479b9efc9145680a69b1
Elyor <e.hadjihanov@gmail.com>
2020-07-30 19:24:01 +0500

    changed from basic to digest auth

--- src/main/java/com/example/api/config/SecurityConfig.java
+++ src/main/java/com/example/api/config/SecurityConfig.java
@@ -1,89 +1,82 @@
 package com.example.api.config;
 
-import org.apache.http.HttpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.web.filter.GenericFilterBean;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import org.springframework.security.web.authentication.www.DigestAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.DigestAuthenticationFilter;
+import org.springframework.stereotype.Component;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    @Qualifier("AuthEntryPoint")
-    private BasicAuthenticationEntryPoint authenticationEntryPoint;
+    private DigestAuthenticationEntryPoint entryPoint;
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user")
-                .password(passwordEncoder().encode("userPass"))
-                .authorities("ROLE_USER");
+    @Override
+    @Bean
+    public UserDetailsService userDetailsServiceBean() {
+        UserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.builder()
+                .username("user")
+                .password("userPass")
+                .roles("USER")
+        .build());
+        return manager;
     }
-
-
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.userDetailsService(userDetailsServiceBean())
+                .authorizeRequests()
                     .anyRequest()
-                    .authenticated()
+                    .hasRole("USER")
                 .and()
-                    .httpBasic()
-                    .authenticationEntryPoint(authenticationEntryPoint)
+                    .exceptionHandling()
+                    .authenticationEntryPoint(entryPoint)
                 .and()
-                    .sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.addFilterAfter(new CustomFilter(), BasicAuthenticationFilter.class);
+                    .addFilterAfter(digestAuthenticationFilter(), BasicAuthenticationFilter.class);
+    }
+
+    public DigestAuthenticationFilter digestAuthenticationFilter() {
+        DigestAuthenticationFilter filter = new DigestAuthenticationFilter();
+        filter.setAuthenticationEntryPoint(entryPoint);
+        filter.setUserDetailsService(userDetailsServiceBean());
+        return filter;
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    public static class CustomFilter extends GenericFilterBean {
-        @Override
-        public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-            filterChain.doFilter(servletRequest, servletResponse);
-        }
-    }
-
-    @Bean(name = "AuthEntryPoint")
-    public BasicAuthenticationEntryPoint authenticationEntryPoint() {
-        return new BasicAuthenticationEntryPoint() {
+    PasswordEncoder passwordEncoder() {
+        return new PasswordEncoder() {
             @Override
-            public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
-                response.addHeader(HttpHeaders.WWW_AUTHENTICATE, "Basic realm=\"" + getRealmName() + "\"");
-                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                PrintWriter writer = response.getWriter();
-                writer.println("HTTP Status 401 - " + authException.getMessage());
+            public String encode(CharSequence charSequence) {
+                return charSequence.toString();
             }
 
             @Override
-            public void afterPropertiesSet() {
-                setRealmName("uzspring");
-                super.afterPropertiesSet();
+            public boolean matches(CharSequence charSequence, String s) {
+                return charSequence.toString().equals(s);
             }
         };
+    }
+
+    @Component
+    public static class CustomDigestAuthenticationEntryPoint extends DigestAuthenticationEntryPoint {
+
+        @Override
+        public void afterPropertiesSet() {
+            setRealmName("uzspring");
+            setKey("6gDXX9jDTiNoXZp24x7X");
+            setNonceValiditySeconds(10);
+            super.afterPropertiesSet();
+        }
     }
 }
